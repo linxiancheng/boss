96730e9a3d5f1c2615ce6e5c6bb458cd69b42ab0
林献程 <296011508@qq.com>
2017-10-13 16:27:39 +0800

    Update style.js

--- js/style.js
+++ js/style.js
@@ -15,7 +15,7 @@ $(function(){
 			url: './js/test.json',
 			dataType: 'json',
 			success: function(data) {
-				console.log(data.xinxi[0]);
+				console.log(data.xinxi[0].dizhi);
 			}
 		})
 	})
