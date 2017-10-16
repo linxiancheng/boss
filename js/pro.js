487db9ec08096f62913a6fb8a16981c1ccadb752
linxianxiancheng <296011508@qq.com>
2017-10-16 19:13:29 +0800

    更改json与内容增加
    更改json与内容增加

--- js/pro.js
+++ js/pro.js
@@ -11,4 +11,37 @@ $(function(){
 	head_box.click(function(){
 		head_box_box.hide();
 	})
+
+//数据显示
+	$.ajax({
+			url: './js/test.json',
+			dataType: 'json',
+			success: function(data) {
+				var src;
+				for (var i = 0; i < data.gongshi.length; i++) {
+					src = `
+					<div class="pro">
+						<a href="javascript:;" class="clearfix">
+							<div class="pro_left">
+								<img src="img/ico-pro.png" alt="pro-logo">
+							</div>
+							<div class="pro_right">
+								<h3>`+ data.xinxi[i].name +`</h3>
+								<p>`+ data.xinxi[i].dizhi +`</p>
+								<p>`+ data.xinxi[i].hangye +`&nbsp;|&nbsp;`+ data.xinxi[i].rongzhi +`&nbsp;|&nbsp;`+ data.xinxi[i].guimo +`</p>
+
+							</div>
+						</a>
+						<a href="javascript:;" class="pro_a">
+							热招：<span class="color1">`+ data.xinxi[i].zhaopinjob.job1 +`</span>等`+ data.xinxi[i].zhaopinjob.length +`个职位<span class="tr">&gt;</span>
+						</a>
+					</div>
+					`;
+					$('.body').append(src);
+				}
+				
+			}
+		})
+
+
 })
