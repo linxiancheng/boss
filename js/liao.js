12d7c3cd60b8d2eee533a29a8924d7b5d92b1584
linxianxiancheng <296011508@qq.com>
2017-10-20 21:41:01 +0800

    键盘问题
    没解决

--- js/liao.js
+++ js/liao.js
@@ -4,14 +4,15 @@ $(function () {
 	head_img1.click(function(event) {
 		history.go(-1);
 	});
-	$('#test').bind('focus', function () {
-		$('.footer').css({
-			"position" : "relative",
-		});
-	})
-	$('#test').bind('blur', function () {
-		$('.footer').css({
-			"position" : "fixed",
-		});
-	})
+	
+	// $('#test').bind('focus', function () {
+	// 	$('.footer').css({
+	// 		"position" : "relative",
+	// 	});
+	// })
+	// $('#test').bind('blur', function () {
+	// 	$('.footer').css({
+	// 		"position" : "fixed",
+	// 	});
+	// })
 })
