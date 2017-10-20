b171f82cb6af7c9cb3af76648a967ad33affb0a4
linxianxiancheng <296011508@qq.com>
2017-10-20 19:46:26 +0800

    聊天文字输入bug
    聊天文字输入bug调试

--- js/liao.js
+++ js/liao.js
@@ -1,6 +1,17 @@
 $(function () {
 	var head_img1 = $('.head_img1');
+
 	head_img1.click(function(event) {
 		history.go(-1);
 	});
+	$('#test').bind('focus', function () {
+		$('.footer').css({
+			"position" : "relative",
+		});
+	})
+	$('#test').bind('blur', function () {
+		$('.footer').css({
+			"position" : "fixed",
+		});
+	})
 })
