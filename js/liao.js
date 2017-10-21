0f31d4d52c2ba6d07a05a7cc8148ee2ffeca902c
linxianxiancheng <296011508@qq.com>
2017-10-21 08:41:05 +0800

    input-bug
    input-bug

--- js/liao.js
+++ js/liao.js
@@ -8,9 +8,11 @@ $(function () {
 	$('#test').bind('focus', function () {
 		$('.body_box').css("margin",0);
 		$('#header').css("position","relative");
+		$('.footer').css("position","relative");
 	})
 	$('#test').bind('blur', function () {
 		$('.body_box').css("margin","2.7rem auto 1.2rem");
 		$('#header').css("position","fixed");
+		$('.footer').css("position","fixed");
 	})
 })
